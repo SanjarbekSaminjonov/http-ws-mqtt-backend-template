52092b44fbb4d45a8a1ed89ba7bb66a2286e649e
Sanjarbek Saminjonov <s.saminjonov@uzinfocom.uz>
2025-12-05 18:09:07 +0500

    feat: Add MQTT user authentication endpoint and update health check

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from main.views import IndexView, health_check
+from main.views import IndexView, check_mqtt_user, health_check
 
 app_name = "main"
 
 urlpatterns = [
     path("health/", health_check, name="health"),
+    path("check-mqtt-user/", check_mqtt_user, name="check_mqtt_user"),
     path("", IndexView.as_view(), name="index"),
 ]
